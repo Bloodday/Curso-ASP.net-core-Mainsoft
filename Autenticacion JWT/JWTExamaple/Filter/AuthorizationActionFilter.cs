caeabf59e5d19b68c9956adca333a47850e9fd86
Jesus Alberto Ramirez Moreno <hck.Bloodday@gmail.com>
2021-10-20 19:49:41 -0500

    Se añadio autenticacion por roles

---
+++ Autenticacion JWT/JWTExamaple/Filter/AuthorizationActionFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTExamaple.Filter
+{
+    public class AuthorizationActionFilter:ActionFilterAttribute
+    {
+        public string[] Roles { set; get; }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.Request.Headers.ContainsKey("Authorization"))
+            {
+                var token = context.HttpContext.Request.Headers["Authorization"].ToString();
+                token = token.Remove(0, 7);
+                var tokenBody = token.Split('.')[1];
+                tokenBody = tokenBody.PadRight(tokenBody.Length + (tokenBody.Length * 3) % 4, '=');  // add padding
+                var bytesTokenBody = Convert.FromBase64String(tokenBody);
+                tokenBody = System.Text.Encoding.UTF8.GetString(bytesTokenBody);
+                var tokenData = JsonConvert.DeserializeObject<dynamic>(tokenBody);
+                if (!Roles.Contains((string)tokenData.role))
+                {
+                    context.Result = new UnauthorizedResult();
+                }
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ Autenticacion JWT/JWTExamaple/JWTExamaple/Startup.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTExamaple
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddCors(options => {
+                options.AddPolicy(name: "AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyHeader();
+                });
+            });
+
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(jwt =>
+            {
+                jwt.RequireHttpsMetadata = false;
+                jwt.SaveToken = true;
+                byte[] secret = System.Text.Encoding.UTF8.GetBytes("askljbsinglsnvhlbanjkvbosenonandnankñfblñandñoanm noan");
+                jwt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(secret),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWTExamaple", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors("AllowAll");
+            app.UseAuthentication();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JWTExamaple v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
